094ec2cff743a08e9770202b833ee4f171a5f662
laurieshinn89 <laurieshinn89@gmail.com>
2026-01-10 10:35:25 +0000

    Add GitHub Pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sigma16-visualiser/',
   plugins: [react()],
   resolve: {
     alias: {
